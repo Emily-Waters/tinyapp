1a3f93d7eb9343593726d79f4f2a8af062422594
Emily-Waters <emilyabigailwaters@gmail.com>
2021-12-06 23:25:33 +0000

    Added post method in express_server

--- express_server.js
+++ express_server.js
@@ -42,6 +42,8 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// app.get("/u/new", (req, res) => {
-//   res.render("urls_new");
-// });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
